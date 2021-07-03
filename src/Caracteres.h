cf9f399803fb1e107288ccb8e56e1789bfd3d6cc
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2021-07-02 22:55:55 -0300

    Adicionada função de criar senha

---
+++ src/Caracteres.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>		
+#include <vector>
+using namespace std;
+
+//enuns consistem em constantes numéricas nomeadas, o que não é a melhor ideia aqui.
+
+class Caracteres{
+    public:
+    const vector<char> minusculas = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    const vector<char> maiusculas ={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+    'Q','R','S','T','U','V','X','Y','Z'};
+    const vector<char> numeros = {'0','1','2','3','4','5','6','7','8','9'};
+    const vector<char> especiais = {'!','?','@','#','%','$','&','(',')','-','+','=','_','*'};
+
+};
+
