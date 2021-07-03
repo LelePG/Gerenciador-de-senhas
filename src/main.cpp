5f3099c54df526f1fd76bbc5fd90cf4cbbb02d7a
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2021-07-02 23:13:32 -0300

    Correação do bug no gerar senha

--- src/main.cpp
+++ src/main.cpp
@@ -1,108 +1,29 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-#include "Caracteres.h"
+#include "Funcoes.h"
 using namespace std;
 
-vector<char> adicionaElementos(vector<char> inserirAqui, vector<char> copiarDaqui)
-{
-    for (vector<char>::iterator it = copiarDaqui.begin(); it != copiarDaqui.end(); it++)
-    {
-        inserirAqui.push_back(*it);
-    }
-    return inserirAqui;
-}
 
-vector<char> selecionarSimbolos(bool letrasMinusculas, bool letrasMaiusculas, bool numeros, bool especiais)
-{
-    Caracteres listaDeLetras;
-    vector<char> selecionados = {};
-
-    if (letrasMaiusculas)
-    {
-        selecionados = adicionaElementos(selecionados, listaDeLetras.maiusculas);
-    }
-
-    if (letrasMinusculas)
-    {
-        selecionados = adicionaElementos(selecionados, listaDeLetras.minusculas);
-    }
-
-    if (numeros)
-    {
-        selecionados = adicionaElementos(selecionados, listaDeLetras.numeros);
-    }
-
-    if (especiais)
-    {
-        selecionados = adicionaElementos(selecionados, listaDeLetras.especiais);
-    }
-
-    return selecionados;
-}
-
-string gerarSenha(int tamanho, bool letrasMinusculas = true, bool letrasMaiusculas = true, bool numeros = true, bool caracteresEspeciais = true)
-{
-    vector<char> componentesSenha = selecionarSimbolos(letrasMinusculas, letrasMaiusculas, numeros, caracteresEspeciais);
-    if (componentesSenha.size() == 0)
-    {
-        return "Houve um problema e sua senha nao foi criada";
-    }
-    string senha;
-    srand((unsigned)time(0)); //inicia a geração de númeroa aleatórios.
-    for (int i = 0; i < tamanho; i++)
-    {
-        int posicao = rand()%(componentesSenha.size());
-        senha.push_back(componentesSenha[posicao]);
-    }
-    return senha;
-}
-
-void imprimirMenu(){
-    cout << "Boas vindas ao gerenciador de senhas!"<<endl;
-    cout<< "Digite a opção que você deseja:"<<endl;
-    cout << "0 - Sair da aplicação" <<endl;
-    cout << "1 - Gerar senha"<< endl;
-}
-
-bool habilitarCampo(string texto){
-    int habilitar;
-    cout << texto << endl;
-    do{
-    cout << "Digite 1 para habilitar, caso contrário, digite 0." << endl;
-    cin >> habilitar;
-    } while ((habilitar != 0) && (habilitar != 1));
-    return habilitar;
-}
 int main()
 {
+    Funcoes f;
+    cout << "Boas vindas ao gerenciador de senhas!" << endl;
     int leitura;
-    do{
-    imprimirMenu();
-    cin >> leitura;
-    switch(leitura){
-        case 0: 
-        break;
+    do
+    {
+        f.imprimirMenu();
+        cin >> leitura;
+        switch (leitura)
+        {
+        case 0:
+            break;
         case 1:
-        int tamanho;
-        bool habilitaMaiuscula, habilitaMinuscula, habilitaNumero, habilitaEspecial;
-        cout << "Qual o tamanho da sua senha?"<<endl;
-        cin >> tamanho;
-        habilitaMaiuscula = habilitarCampo("Quer habilitar letras maiusculas?");
-                habilitaMinuscula = habilitarCampo("Quer habilitar letras minusculas?");
-        habilitaNumero = habilitarCampo("Quer habilitar numeros?");
-                habilitaEspecial = habilitarCampo("Quer habilitar caracteres especiais?");
-
-
-        cout << "Sua senha é: " << gerarSenha(tamanho, habilitaMaiuscula, habilitaMinuscula, habilitaNumero, habilitaEspecial) << endl;
-        break;
+            f.criarSenha();
+            break;
 
         default:
-        cout << "Opção inválida" << endl;
-    }
+            cout << "Opção inválida" << endl;
+        }
 
-    }while(leitura != 0);
+    } while (leitura != 0);
     return 0;
 }
